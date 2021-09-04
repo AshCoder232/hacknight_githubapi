f2c5c9a10660e52106e7ab013d06891b04bbac4b
fausan123 <fausan.asharaf01@gmail.com>
2021-09-04 19:05:06 +0530

    template folder added

--- repos/views.py
+++ repos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+import requests
+import random
 # Create your views here.
+
+def index(request):
+    random_page = random.randint(0, 100000)
+    response = requests.get('https://api.github.com/repositories', params={'since': random_page})
+    data = response.json()
+    random_repos = random.sample(data, 5)
+    del_cols = ['']
+    for i in range(0, 5):
+        url = random_repos[i]['url']
+        r = requests.get(url)
+        d = r.data()
+    return render(request, "repos/index.html")
