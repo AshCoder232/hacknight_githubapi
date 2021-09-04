3222e4d7c34f40ddd59d6c1fec9ffbd1d69319da
fausan123 <fausan.asharaf01@gmail.com>
2021-09-05 00:58:35 +0530

    updated view

--- repos/views.py
+++ repos/views.py
@@ -4,7 +4,7 @@ import random
 import os
 # Create your views here.
 
-TOKEN = os.environ.get('GITHUB_ACCESS_TOKEN')
+TOKEN = os.environ.get('GITHUB_ACCESS_TOKEN', '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')
 
 def index(request):
     selected_repos = []
@@ -17,7 +17,7 @@ def index(request):
             random_repos = random.sample(data, 10)
             for i in range(0, 10):
                 print(i)
-                r = requests.get(random_repos[i]['url'])
+                r = requests.get(random_repos[i]['url'], headers={'Authorization': f"token {TOKEN}"})
                 d = r.json()
                 if (r.status_code != 200):
                     break
@@ -26,7 +26,7 @@ def index(request):
                     random_repos[i]['watchers'] = d['watchers_count']
                     random_repos[i]['forks'] = d['forks_count']
 
-                    lr = requests.get(d['languages_url'])
+                    lr = requests.get(d['languages_url'], headers={'Authorization': f"token {TOKEN}"})
                     random_repos[i]['languages'] = list(lr.json().keys())              
                     selected_repos.append(random_repos[i])
                     break
